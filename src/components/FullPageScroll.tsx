fd4bd593cdfef7cff54353193725c42c616cda7a
Kayvia Harriott <55689354+KayviaHarriott@users.noreply.github.com>
2024-04-17 11:40:36 -0500

    Added pages with text and background colors to implement full page scroll

---
+++ src/components/FullPageScroll.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+
+interface FullPageScrollProps{
+    sections: React.ReactNode[];
+}
+export const FullPageScroll: FC<FullPageScrollProps> = ({sections}) => {
+
+    return (
+        <>
+            <p>Scroll</p>
+        </>
+       
+    )
+}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,71 +1,38 @@
-import { Box, Button, InputLabel, TextField } from "@mui/material";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import React, { useEffect, useState } from "react";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { InputLabelStyling } from "../styles/ComponentStyling";
-import { useNavigate } from "react-router-dom";
-import { Logo } from "../components/Logo";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import React from "react";
+import { FullPageScroll } from "../components/FullPageScroll";
+import { motion, useScroll } from "framer-motion";
+import { Box } from "@mui/system";
 
 export const LandingPage = () => {
-  const navigate = useNavigate();
-  const [trn, setTRN] = useState<string>("");
-  const [controlNumber, setControlNumber] = useState<string>("");
-  const [licenseIssue, setLicenseIssue] = useState<string>("");
-  const [dateOfBirth, setDateOfBirth] = useState<string>("");
-  const handleNavigation = () => {
-    navigate("/ticket-information", {
-      state: { data: { trn, controlNumber, licenseIssue, dateOfBirth } },
-    });
-  };
-  useEffect(() => {
-    document.title = "Kay the Dev | Kayvia Harriott"; // Change 'New Title' to whatever you want
-  }, []);
+  const m = motion;
+  const { scrollYProgress } = useScroll();
+
+  const list = [
+    { label: "One", backgroundColor: "lightgreen" },
+    { label: "Two", backgroundColor: "lightblue" },
+    { label: "Three", backgroundColor: "lightpink" },
+  ];
   return (
     <>
-      <Box
-        sx={{ backgroundColor: "#E6F2F6" }}
-        className="flex flex-col justify-center items-center h-screen gap-3 "
+      {" "}
+      {/* <motion.div /> */}
+      <m.div
+        //  style={{ scaleX: scrollYProgress }}
+        // initial={{ opacity: 0, scale: 0.5 }}
+        // animate={{ opacity: 1, scale: 1 }}
+        // transition={{ duration: 1 }}
       >
-        <Box
-          sx={{
-            width: "fit-content",
-            boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-            backgroundColor: "white",
-            padding: 4,
-            borderRadius: 2,
-          }}
-          className="text-center"
-        >
-          <p className="text-[32px] font-bold text-[#087CA7]">
-            Kayvia Harriott
-          </p>
-          <div className="flex flex-col gap-2 w-[400px] text-center">
-            <p className="text-[gray]">
-              Hi! I'm currently working on my website, but in the meanwhile,
-              contact me below:
-            </p>
-            <div className="flex flex-col text-center items-center justify-center gap-1">
-              <a href="mailto:kayvia@kaythedev.com">
-                <div className="flex gap-1 items-center justify-center text-center">
-                  <MailOutlineIcon />
-                  <p className="font-bold">kayvia@kaythedev.com</p>
-                </div>
-              </a>
-              <a
-                href="https://www.instagram.com/kayviaharriott"
-                target="_blank"
-              >
-                <div className="flex gap-1 items-center justify-center text-center">
-                  <InstagramIcon />
-                  <p className="font-bold">kayviaharriott</p>
-                </div>
-              </a>
-            </div>
-          </div>
-        </Box>
-      </Box>
+        {list.map((item, index) => (
+          <Box
+            sx={{ backgroundColor: `${item.backgroundColor}` }}
+            className={`h-screen flex items-center justify-center`}
+            key={index}
+          >
+            {item.label}
+          </Box>
+        ))}
+      </m.div>
+      {/* <FullPageScroll sections={[]} /> */}
     </>
   );
 };
