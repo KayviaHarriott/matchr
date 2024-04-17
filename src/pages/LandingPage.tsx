ad5a45341d3059907b1268c586edff6cd83de63b
Kayvia Harriott <55689354+KayviaHarriott@users.noreply.github.com>
2024-04-17 12:13:14 -0500

    Added face in on page load

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FullPageScroll } from "../components/FullPageScroll";
 import { motion, useScroll } from "framer-motion";
 import { Box } from "@mui/system";
 
 export const LandingPage = () => {
   const m = motion;
-  const { scrollYProgress } = useScroll();
+
+  const { scrollYProgress, scrollXProgress } = useScroll();
 
   const list = [
     { label: "One", backgroundColor: "lightgreen" },
     { label: "Two", backgroundColor: "lightblue" },
     { label: "Three", backgroundColor: "lightpink" },
   ];
+
   return (
     <>
-      {" "}
-      {/* <motion.div /> */}
       <m.div
-        //  style={{ scaleX: scrollYProgress }}
-        // initial={{ opacity: 0, scale: 0.5 }}
-        // animate={{ opacity: 1, scale: 1 }}
-        // transition={{ duration: 1 }}
+        initial={{ opacity: 0 }} 
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
       >
+        <m.div>
+          <Box
+            sx={{ backgroundColor: "lightYellow" }}
+            className={`h-screen flex items-center justify-center`}
+          >
+            <p>Section 1</p>
+          </Box>
+        </m.div>
         {list.map((item, index) => (
           <Box
             sx={{ backgroundColor: `${item.backgroundColor}` }}
