ff2c377e18f988bc4d272a518b1600c18c7424cf
Kayvia Harriott <55689354+KayviaHarriott@users.noreply.github.com>
2024-04-17 12:32:25 -0500

    Modified to tailwind css for section

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,45 +1,57 @@
-import React, { useEffect } from "react";
-import { FullPageScroll } from "../components/FullPageScroll";
-import { motion, useScroll } from "framer-motion";
-import { Box } from "@mui/system";
+// Photos from https://citizenofnowhe.re/lines-of-the-city
+import "./styles.css";
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  MotionValue,
+} from "framer-motion";
+import React from "react";
+
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
+
+function Image({ id }: { id: number }) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useParallax(scrollYProgress, 300);
+
+  return (
+    <div
+      id="section"
+      className="h-[100vh] flex justify-center items-center relative perspective-500"
+    >
+      <div ref={ref}>
+        <div>
+          {" "}
+          <p className="text-[blue]">kk</p>
+        </div>
+        {/* <img src={`/${id}.jpg`} alt="A London skyscraper" /> */}
+      </div>
+      <motion.h2 style={{ y }}>{`#00${id}`}</motion.h2>
+    </div>
+  );
+}
 
 export const LandingPage = () => {
-  const m = motion;
-
-  const { scrollYProgress, scrollXProgress } = useScroll();
-
-  const list = [
-    { label: "One", backgroundColor: "lightgreen" },
-    { label: "Two", backgroundColor: "lightblue" },
-    { label: "Three", backgroundColor: "lightpink" },
-  ];
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
 
   return (
     <>
-      <m.div
-        initial={{ opacity: 0 }} 
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 1.5 }}
-      >
-        <m.div>
-          <Box
-            sx={{ backgroundColor: "lightYellow" }}
-            className={`h-screen flex items-center justify-center`}
-          >
-            <p>Section 1</p>
-          </Box>
-        </m.div>
-        {list.map((item, index) => (
-          <Box
-            sx={{ backgroundColor: `${item.backgroundColor}` }}
-            className={`h-screen flex items-center justify-center`}
-            key={index}
-          >
-            {item.label}
-          </Box>
-        ))}
-      </m.div>
-      {/* <FullPageScroll sections={[]} /> */}
+      {[1, 2, 3, 4, 5].map((image) => (
+        <p>
+          <Image id={image} />
+        </p>
+      ))}
+      <motion.div className="progress" style={{ scaleX }} />
     </>
   );
 };
