c97a3d67290fc119638be350b12449eb9c0a77d6
Kayvia Harriott <55689354+KayviaHarriott@users.noreply.github.com>
2024-04-17 13:24:45 -0500

    Added stylign for About section to determine layout

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,7 +8,7 @@ import {
   MotionValue,
 } from "framer-motion";
 import { useRef } from "react";
-
+import { Instagram, GitHub } from "@mui/icons-material";
 export const About = () => {
   const ref = useRef(null);
 
@@ -22,29 +22,47 @@ export const About = () => {
   return (
     <>
       <Box className="flex px-[32px] gap-4">
-        <Box className="max-w-[300px]">
-          <p
-            style={{ scrollSnapAlign: "center" }}
-            className="text-[32px] font-bold"
-          >
-            kaythedev
-          </p>
+        <Box className="max-w-[300px] text-right flex flex-col gap-1">
+          <div>
+            <p
+              style={{ scrollSnapAlign: "center" }}
+              className="text-[32px] font-bold"
+            >
+              kaythedev
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            {["software engineer", "web developer"].map((items, key) => (
+              <Box
+                sx={{ borderRadius: 1 }}
+                className="bg-[black] text-[white] text-[14px] flex justify-center items-center py-2 px-2"
+              >
+                <p>{items}</p>
+              </Box>
+            ))}
+          </div>
+          <p></p>
         </Box>
-        <Box className="max-w-[350px]">
-        <div className="flex flex-col gap-2">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-                convallis urna nec turpis aliquet, non pellentesque orci interdum.
-                Aliquam ut est ante. Aliquam ac dui eu orci faucibus rutrum. Etiam
-                porttitor est turpi.
-              </p>
-              <p>
-                Vitae varius ex faucibus in. Maecenas non consectetur justo.
-                Suspendisse arcu enim, maximus at accumsan sit amet, cursus eget ex.
-                Pellentesque in ex ac leo blandit euismod. Phasellus porta finibus
-                imperdiet.
-              </p>
-        </div>
+        <Box className="max-w-[350px] sm:max-w-[400px]">
+          <div className="flex flex-col gap-2">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              convallis urna nec turpis aliquet, non pellentesque orci interdum.
+              Aliquam ut est ante. Aliquam ac dui eu orci faucibus rutrum. Etiam
+              porttitor est turpi.
+            </p>
+            <p>
+              Vitae varius ex faucibus in. Maecenas non consectetur justo.
+              Suspendisse arcu enim, maximus at accumsan sit amet, cursus eget
+              ex. Pellentesque in ex ac leo blandit euismod. Phasellus porta
+              finibus imperdiet.
+            </p>
+          </div>
+          <div className="pt-4 flex flex-row gap-2 items-center justify-center text-center">
+            <GitHub />
+            <Instagram />
+          </div>
+          <div></div>
         </Box>
       </Box>
     </>
