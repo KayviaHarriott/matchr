2cc5072f32844706508053cebb1aca00212242e5
Kayvia Harriott <55689354+KayviaHarriott@users.noreply.github.com>
2024-04-18 15:54:41 -0500

    Added second section for how it works and footer

---
+++ matchr/src/pages/Footer.tsx
@@ -0,0 +1,17 @@
+export const Footer = () => {
+  return (
+    <div className="w-screen bg-[black] p-[32px] flex items-center justify-center">
+      <div className="flex gap-8">
+        <div className="">
+          <p className="text-[white] font-bold text-[32px]">Matchr</p>
+        </div>
+        <div className="pt-3">
+          <p className="text-[white] font-bold text-[20px]">Quick Links</p>
+        </div>
+        <div className="pt-3">
+          <p className="text-[white] font-bold text-[20px]">Socials</p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- matchr/src/pages/LandingPage.tsx
+++ matchr/src/pages/LandingPage.tsx
@@ -1,28 +1,27 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, Icon } from "@mui/material";
 // import couple_image from "/imgs/header-image.jpg";
-import bottomDivider from "/imgs/dividers/wavesOpacity_bottom.svg";
 import { motion } from "framer-motion";
-import Wave from "react-wavify";
+import { Footer } from "./Footer";
+import Figma from "feather-icons"
+// import MessageSquare from "feather-icons"
 
 export const LandingPage = () => {
   const m = motion;
   return (
     <div className="">
-      <Box sx={{ paddingY: 8, backgroundColor: "#AA6BD4" }}></Box>
-      <Box className="bg-[transparent]">
-        <img src={bottomDivider} />
-        <Box className="flex justify-center bg-transparent">
+      <Box className="py-[32px]">
+        <Box className="flex justify-center">
           <m.div
-            animate={{ rotate: [0, 1, 0, 2, 0] }}
-            transition={{ duration: 8, repeat: Infinity }}
-            style={{ display: "inline-block" }}
+          // animate={{ rotate: [0, 1, 0, 2, 0] }}
+          // transition={{ duration: 8, repeat: Infinity }}
+          // style={{ display: "inline-block" }}
           >
             <Box
               sx={{
                 backgroundColor: "white",
-                border: 1,
-                borderColor: "rgba(203,156,241,0.1)",
-                boxShadow: "4px 4px 10px 0px rgba(203,156,241,0.2)",
+                // border: 1,
+                // borderColor: "rgba(203,156,241,0.1)",
+                // boxShadow: "4px 4px 10px 0px rgba(203,156,241,0.2)",
                 px: 3,
                 pb: 4,
                 pt: 2,
@@ -84,37 +83,47 @@ export const LandingPage = () => {
             </Box>
           </m.div>
         </Box>
-        <Box className="relative">
-          <Wave
-            className="absolute mt-[24px]"
-            fill="#AA6BD4"
-            paused={false}
-            style={{ display: "flex" }}
-            options={{
-              height: 10,
-              amplitude: 15,
-              speed: 0.2,
-              points: 2,
-            }}
-          />
-          <Wave
-            className="mt-[24px]"
-            fill="#AA6BA4"
-            paused={false}
-            style={{ display: "flex" }}
-            options={{
-                
-              height: 10,
-              amplitude: 20,
-              speed: 0.3,
-              points: 3,
-            }}
-          />
+      </Box>
+      <Box className="py-[32px] px-[15%] bg-[#EFEBF8]">
+        <Box className="flex">
+          <Box>
+            <p>How it Works</p>
+          </Box>
+          <Box>
+          <Box
+              sx={{
+                backgroundColor: "white",
+                border: 1,
+                borderColor: "rgba(203,156,241,0.1)",
+                boxShadow: "4px 4px 10px 0px rgba(203,156,241,0.2)",
+                px: 3,
+                pb: 4,
+                pt: 2,
+                borderRadius: 2,
+              }}
+              className=""
+            >
+              <div className="flex flex-col gap-4">
+                {/* <p className="text-[13px] mb-[0px] mb-[-16px]">Duis fringilla</p> */}
+                <div className="flex flex-col gap-1">
+                 <p>Icon</p>
+                 
+                  <p className="max-w-[400px] text-[black] font-bold">
+                    Step 1
+                  </p>
+                  <p className="max-w-[400px] text-[black] font-light">
+                    Nunc sem eros, pulvinar vel consectetur eget, molestie vel
+                    nibh. Quisque venenatis aliquam neque non ultrices. Aliquam
+                    vitae enim erat. Duis ultricies id tortor ut condimentum.
+                    Aliquam nec consectetur elit.
+                  </p>
+                </div>
+              </div>
+            </Box>
+          </Box>
         </Box>
       </Box>
-      <Box
-        sx={{ height: "32px", marginTop: "-1px", backgroundColor: "#AA6BD4" }}
-      ></Box>
+      <Footer />
     </div>
   );
 };
